5ccd272b718205b9382a2d0e74e6fb1b4258181c
Gugiet <mguzowski92@gmail.com>
2023-11-19 13:51:45 +0100

    commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { Route, Switch } from 'react-router-dom';
+import Navigation from './components/Navigation/Navigation';
+import { lazy, Suspense } from 'react';
+import Container from './components/Container/Container';
+import Loader from 'components/Loader/Loader';
+
+const HomePage = lazy(() => import('./pages/HomePage'));
+const MoviesPage = lazy(() => import('./pages/MoviesPage'));
+const MovieDetailsPage = lazy(() =>
+  import('./pages/MovieDetailsPage/MovieDetailsPage')
+);
+const NotFoundView = lazy(() => import('./pages/NotFoundView'));
+
+export default function App() {
+  return (
+    <Container>
+      <Navigation />
+      <Suspense fallback={<Loader />}>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/movies">
+            <MoviesPage />
+          </Route>
+          <Route path="/movies/:movieId">
+            <MovieDetailsPage />
+          </Route>
+
+          <Route>
+            <NotFoundView path="*" />
+          </Route>
+        </Switch>
+      </Suspense>
+    </Container>
+  );
+}
---
+++ src/pages/MoviesPage.js
@@ -0,0 +1,95 @@
+import s from './MoviesPage.module.css';
+
+import { useState, useEffect } from 'react';
+import { searchMovies } from '../services/movies-api';
+import { Link, useLocation, useHistory } from 'react-router-dom';
+import Notiflix from 'notiflix';
+
+const MoviesPage = () => {
+  const [movieToFind, setMovieToFind] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  const location = useLocation();
+  const history = useHistory();
+
+  useEffect(() => {
+    const searchString = new URLSearchParams(location.search).get('query');
+
+    if (searchString) {
+      const getMovies = async () => {
+        const { results } = await searchMovies(searchString);
+
+        setMovies(results);
+        setMovieToFind(searchString);
+
+        console.log(searchString);
+      };
+
+      getMovies();
+    }
+  }, [location.search]);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    if (movieToFind.trim()) {
+      const { results } = await searchMovies(movieToFind);
+
+      setMovies(results);
+      setMovieToFind('');
+
+      if (results.length === 0) {
+        Notiflix.Notify.warning(
+          'No movies found! Please change your request and try again'
+        );
+      }
+
+      history.push({
+        ...location,
+        search: `query=${movieToFind}`,
+      });
+    }
+  };
+
+  return (
+    <>
+      <header className={s.searchbar}>
+        <form className={s.searchForm} onSubmit={handleSubmit}>
+          <input
+            onChange={e => setMovieToFind(e.target.value)}
+            className={s.SearchFormInput}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search movie"
+            value={movieToFind}
+          />
+          <button type="submit" className={s.searchFormButton}>
+            search
+          </button>
+        </form>
+      </header>
+      {movies.length > 0 &&
+        movies.map(({ id, title, poster_path }) => (
+          <ul>
+            <li key={id}>
+              <Link
+                to={{
+                  pathname: `/movies/${`${id}`}`,
+                  state: {
+                    from: {
+                      location,
+                    },
+                  },
+                }}
+              >
+                <p>{title}</p>
+              </Link>
+            </li>
+          </ul>
+        ))}
+    </>
+  );
+};
+
+export default MoviesPage;
